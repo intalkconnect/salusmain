d44c218f6b8d4c7b82d53622501c95b436451a11
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-22 11:20:24 -0300

    Update uploadLocalWorker.js

--- workers/uploadLocalWorker.js
+++ workers/uploadLocalWorker.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const fs = require("fs").promises;
 const path = require("path");
 const { supabase } = require("../src/utils/supabaseClient");
-const { log, error } = require("../utils/logger");
+const { log, error } = require("../src/utils/logger");
 const cron = require("node-cron");
 
 // Configurações
