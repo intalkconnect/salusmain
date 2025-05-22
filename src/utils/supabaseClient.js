d315b53c6129a481b9c068886f7ecb9ed2550ad9
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-22 19:42:08 -0300

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -2,8 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
-const supabaseBucket = process.env.SUPABASE_BUCKET;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-module.exports = { supabase, supabaseBucket };
+module.exports = { supabase };
