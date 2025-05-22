68ea098dd8d28302a655470836bcd8708a6f532a
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-21 22:27:34 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       name: 'worker',
       script: 'workers/worker.js',
-      instances: 'max',
-      exec_mode: 'cluster',
+      instances: '4',
+      exec_mode: 'fork',
     },
   ],
 };
