6fa05335d2b252662cbd51539c0d0739cae5b3ff
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-22 12:21:22 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,19 @@ module.exports = {
     {
       name: 'api',
       script: 'index.js',
-      instances: 'max',
+      instances: '2',
       exec_mode: 'cluster',
     },
     {
       name: 'worker-upload-local',
       script: 'workers/uploadLocalWorker.js',
-      instances: '1',
+      instances: '3',
       exec_mode: 'fork',
     },
     {
       name: 'worker-process-job',
       script: 'workers/processJobWorker.js',
-      instances: '1',
+      instances: '4',
       exec_mode: 'fork',
     },
     {
