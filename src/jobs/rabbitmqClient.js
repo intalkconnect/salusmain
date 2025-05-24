9477c0691760c2c824c6edc0016be830e5858cf0
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-24 16:03:48 -0300

    Update rabbitmqClient.js

--- src/jobs/rabbitmqClient.js
+++ src/jobs/rabbitmqClient.js
@@ -1,6 +1,6 @@
 const amqp = require('amqplib');
 
-const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://localhost';
+const RABBITMQ_URL = process.env.RABBITMQ_URL;
 
 async function sendToQueue(queue, message) {
   const connection = await amqp.connect(RABBITMQ_URL);
